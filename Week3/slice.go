112331a343175c4328f44c956b52f809ed92ffde
Max Ahlberg <maxahlberg@hotmail.com>
2020-04-14 23:27:37 +0200

    Add alot

--- Week3/slice.go
+++ Week3/slice.go
@@ -12,14 +12,12 @@ func main() {
 	fmt.Println("Insert a number: ")
 	inputReader := bufio.NewReader(os.Stdin)
 	input, err := inputReader.ReadString('\n')
-	fmt.Printf("What is this? %T \n", input)
 	if err != nil {
 		fmt.Printf("error while scanning: %v \n ", err)
 	}
-	if s, err := strconv.Atoi(input); err != nil {
-		fmt.Printf("%T, %v", s, s)
-	}
-	fmt.Printf("the error %v \n", err )
-	//fmt.Printf("The input is: %v \n ", s)
-	fmt.Printf("the input without parsing is %v \n", input)
+	fmt.Printf("What is this? %T, saying: %s  \n", input, input)
+	s, _ := strconv.Atoi(input)
+	fmt.Printf("Then I try to convert to: %T, But the value is:  %v \n", s, s)
+	fmt.Printf("the error %v \n", err)
+	fmt.Printf("the input without parsing is, a %T with value  %v \n", input, input)
 }
